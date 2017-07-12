4303dfde818f35368ada0535309458e4a120e513
Sarah Kirk <kirk710@gmail.com>
2017-07-12 10:33:31 -0600

    doing some research

--- app/services/propublica_service.rb
+++ app/services/propublica_service.rb
@@ -4,6 +4,9 @@ class PropublicaService
   def initialize
     @connection = Faraday.new('https://api.propublica.org/congress/v1/')
   end
+  # List of members: GET https://api.propublica.org/congress/v1/{congress}/{chamber}/members.json
+  # Specific member: GET https://api.propublica.org/congress/v1/members/{member-id}.json
+  # current members by state: GET https://api.propublica.org/congress/v1/members/{chamber}/{state}/{district}/current.json
 
   def house_members(info)
     parse(connection.get('house_members', info))[:results]
